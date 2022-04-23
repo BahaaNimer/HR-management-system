fe205f0bb8bade60a8af6c7caffdca34f03d33a7
BahaaNimer <bahaanimer97@gmail.com>
2022-04-23 18:40:48 +0300

    add submit form

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,69 @@
 'use strcit'
 
-function Employee(id, name, department, level, image) {
-  this.employeeId = id,
-    this.fullName = name,
-    this.department = department,
-    this.level = level,
-    this.imageUrl = image
-  // this.salary = salary
+allEmployees = [];
+let forms = document.getElementById('form')
+let card = document.getElementById('cadDiv')
+let sectionEl = document.getElementById('cardSection')
+
+// constructor
+function Employee(id, name, department, level, image, salary) {
+  this.employeeId = id;
+  this.fullName = name;
+  this.department = department;
+  this.level = level;
+  this.image = image;
+  this.salary = salary;
+  allEmployees.push(this);
+
 }
+
+Employee.prototype.render = function () {
+  let card = document.createElement('div')
+  card.classList = ('cardDiv')
+  sectionEl.appendChild(card)
+  // create image for employee
+  let imageEl = document.createElement('img');
+  imageEl.classList = ('card-img-top');
+  imageEl.src = this.image;
+  imageEl.alt = 'profilePic';
+  card.appendChild(imageEl);
+
+  // create name for employee
+  let name = document.createElement('h5');
+  name.classList = ('card-title');
+  name.textContent = this.fullName;
+  card.appendChild(name);
+
+  // create Department, Level, salary, Id for employee
+  let paragraph = document.createElement('p');
+  paragraph.classList = ('card-text');
+  paragraph.textContent = `ID: ${this.employeeId}  \n - Department: ${this.department} \n - Level: ${this.level} \n - salary: ${this.salary} JD`;
+  card.appendChild(paragraph);
+};
+
+Employee.prototype.id = function () {
+  return Math.floor(1000 + Math.random() * 9000);
+
+};
+
+// create a new employee
+
+let employee_1 = new Employee('1000', 'Ghazi Samer', 'Administration', 'Senior', "../assets/Ghazi.jpg", '1700');
+let employee_2 = new Employee('1001', 'Lana Ali', 'Finance', 'Senior', '../assets/Lana.jpg', '1500');
+let employee_3 = new Employee('1002', 'Tamara Ayoub', 'Marketing', 'Senior', '../assets/Tamara.jpg', '1900');
+let employee_4 = new Employee('1003', 'Safi Walid', 'Administration', 'Mid-Senior', '../assets/Safi.jpg', '1300');
+let employee_5 = new Employee('1004', 'Omar Zaid', 'Development', 'Senior', `../assets/Omar.jpg`, '2000');
+let employee_6 = new Employee('1005', 'Rana Saleh', 'Development', 'Junior', '../assets/Rana.jpg', '700');
+let employee_7 = new Employee('1006', 'Hadi Ahmad', 'Finance', 'Mid-Senior', '../assets/Hadi.jpg', '1400');
+
+
+
+
+
+for (let i = 0; i < allEmployees.length; i++) {
+  allEmployees[i].render();
+}
+
 
 Employee.prototype.salary = function () {
   let net = 0;
@@ -26,25 +82,26 @@ Employee.prototype.salary = function () {
   } return `${net - (net * (7.5 / 100))}`;
 };
 
-Employee.prototype.render = function () {
-  // return document.getElementById('container').innerHTML = `Employee name is: ${this.fullName} and the salary is: ${this.salary()}`;
-  // document.write.getElementById('container')(`Employee name is: ${this.fullName} and the salary is: ${this.salary()}<br>`)
-  return `Employee name is: ${this.fullName} and the salary is: ${this.salary()}<br>`
+
+// create a new Event Listener
+
+forms = addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  console.log(event);
+  let id = Employee.prototype.id();
+  let fullName = event.target.floatingInput1.value;
+  let department = event.target.floatingSelect1.value;
+  let level = event.target.floatingSelect2.value;
+  let image = event.target.formFile.value;
+  let salary = Employee.prototype.salary();
+
+
+  let newEmployee = new Employee(id, fullName, department, level, image, salary);
+
+  newEmployee.render();
+
 };
 
-let employee_1 = new Employee('1000', 'Ghazi Samer', 'Administration', 'Senior');
-let employee_2 = new Employee('1001', 'Lana Ali', 'Finance', 'Senior');
-let employee_3 = new Employee('1002', 'Tamara Ayoub', 'Marketing', 'Senior');
-let employee_4 = new Employee('1003', 'Safi Walid', 'Administration', 'Mid-Senior');
-let employee_5 = new Employee('1004', 'Omar Zaid', 'Development', 'Senior');
-let employee_6 = new Employee('1005', 'Rana Saleh', 'Development', 'Junior');
-let employee_7 = new Employee('1006', 'Hadi Ahmad', 'Finance', 'Mid-Senior');
-
-
-document.getElementById('container1').innerHTML = `${employee_1.render()}`;
-document.getElementById('container2').innerHTML = `${employee_2.render()}`;
-document.getElementById('container3').innerHTML = `${employee_3.render()}`;
-document.getElementById('container4').innerHTML = `${employee_4.render()}`;
-document.getElementById('container5').innerHTML = `${employee_5.render()}`;
-document.getElementById('container6').innerHTML = `${employee_6.render()}`;
-document.getElementById('container7').innerHTML = `${employee_7.render()}`;
